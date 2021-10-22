abbe86edd32bc0c77d4bf4fa0990c40958d73775
skg <skaseyg@gmail.com>
2021-10-22 18:47:54 -0400

    renamed local branch from master to main

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+console.log("script loaded");
+
+// DEPENDENCIES =================================
+// connect to save buttons
+// connect to text areas
+// connect to header
+// connect to moment.js
+
+// DATA =========================================
+// current time to print to screen (header)
+
+// FUNCTIONS ====================================
+// master functions -----------------------------
+// start app
+// helper functions -----------------------------
+// get current time (moment)
+// save to local storage
+// getting from local storage
+// stretch: clear local storage
+// based on time -> change color of text areas
+
+// USER INTERACTIONS ============================
+// [event listeners]
+// save buttons -> local storage
+
+// INITIALIZATION ===============================
+// start app?
